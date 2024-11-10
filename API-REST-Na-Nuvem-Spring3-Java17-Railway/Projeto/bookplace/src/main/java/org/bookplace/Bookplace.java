453ea710e6fe26903173ed813e72be7cb46f412a
FrancisBFTC <bftcorporations@gmail.com>
2024-11-10 16:33:26 -0300

    Ainda é necessário corrigir erros de Deploy e .env

--- API-REST-Na-Nuvem-Spring3-Java17-Railway/Projeto/bookplace/src/main/java/org/bookplace/Bookplace.java
+++ API-REST-Na-Nuvem-Spring3-Java17-Railway/Projeto/bookplace/src/main/java/org/bookplace/Bookplace.java
@@ -1,13 +1,30 @@
 package org.bookplace;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
-public class Bookplace {
+public class Bookplace implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bookplace.class, args);
 	}
+
+	@Override
+    public void run(String... args) throws Exception {
+        // Carrega o arquivo .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Aqui você pode acessar as variáveis de ambiente
+        String dbUrl = dotenv.get("PGHOST");
+        String dbPassword = dotenv.get("PGPASSWORD");
+
+        System.out.println("\n\n\nDB URL: " + dbUrl);
+        System.out.println("\n\n\nDB Username: " + dbPassword);
+        // A senha geralmente não se imprime por segurança
+    }
 
 }
