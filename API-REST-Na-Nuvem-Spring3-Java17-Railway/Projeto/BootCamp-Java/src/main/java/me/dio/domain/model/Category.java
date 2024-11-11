59eec7afb726e7530a886a9dccc9f51e117e6e67
FrancisBFTC <bftcorporations@gmail.com>
2024-11-11 00:52:25 -0300

    feat: Altera HELP.md e adicionar arquivo JSON

--- API-REST-Na-Nuvem-Spring3-Java17-Railway/Projeto/BootCamp-Java/src/main/java/me/dio/domain/model/Category.java
+++
@@ -1,8 +0,0 @@
-package me.dio.domain.model;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "tb_category")
-public class Category extends BookType {
-    
-}
--- API-REST-Na-Nuvem-Spring3-Java17-Railway/Projeto/BootCamp-Java/src/main/java/me/dio/domain/model/Tag.java
+++
@@ -1,8 +0,0 @@
-package me.dio.domain.model;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "tb_tag")
-public class Tag extends BookType{
-
-}
